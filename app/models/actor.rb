b5f7202a00a1cfe433dfbc00a10e1a08769d278e
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-03 00:37:42 +0300

    adding list_roles

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+    # define the assocations
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    characters.each do |character|
+      puts "#{character.name} - #{character.show.title}"
+    end
+  end
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
+    # establish an association
+    belongs_to :actor
+    belongs_to :show
+
+    # method
+    def say_that_thing_you_say
+        puts "{name}: #{catchphrase}"
+    end
+
   
 end
