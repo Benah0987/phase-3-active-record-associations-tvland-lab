90aedbbf221affbcb5eefaeee86ff0fcdf59ca6b
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-03 00:04:03 +0300

    completed

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,17 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    create_table :characters do |t|
+      #  character should have a name
+      t.string :name
+      # actor_id (integer)
+      t.integer :actor_id
+      # show_id (integer).
+      t.integer :show_id
+
+      t.timestamps
+    end
+
+    add_index :characters, :actor_id
+    add_index :characters, :show_id
   end
 end
